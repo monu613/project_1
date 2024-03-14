d823e48442d0a758d5302f1f1d489a84fb70d30f
monu613 <108486812+monu613@users.noreply.github.com>
2024-03-14 19:04:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,15 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+db = SQLAlchemy()
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(100))
+    bucket = db.Column(db.String(100))
+    region = db.Column(db.String(100))
+    url = db.Column(db.String(1000))  # New column to store the pre-signed URL
+
 @app.route('/upload', methods=['POST'])
 def handle_upload():
     try:
@@ -23,9 +32,26 @@ def handle_upload():
         if file and allowed_file(file.filename):
             # Generate a unique filename
             new_filename = os.path.join('uploads', os.urandom(24).hex() + os.path.splitext(file.filename)[1])
-            
+            bucket_name = "intital-code-bucket"  # Replace with your S3 bucket name
             # Upload the file to S3
-            s3.upload_fileobj(file, 'intial-code-bucket', new_filename)
+            try:
+                # IAM role is used here for AWS credentials
+                #s3.upload_fileobj(uploaded_file, bucket_name, new_filename)
+                # Generate a pre-signed URL for the uploaded file
+                file_url = s3.generate_presigned_url('get_object',
+                                                     Params={'Bucket': bucket_name,
+                                                             'Key': new_filename},
+                                                     ExpiresIn=3600)  # Link expires in 1 hour
+            except NoCredentialsError:
+                return "Credentials are not available for AWS S3."
+
+            file = File( filename=new_filename,
+                        bucket=bucket_name, region="ap-south-1", url=file_url)
+
+            db.session.add(file)
+            db.session.commit()
+
+            #return redirect(url_for("index"))
             
             # Trigger the Lambda function
             trigger_lambda_function(new_filename)
@@ -47,5 +73,4 @@ def trigger_lambda_function(file_name):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html',files=files)
