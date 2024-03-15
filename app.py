ed07b12dc1dbb749f882fea507d041ea453052f0
monu613 <108486812+monu613@users.noreply.github.com>
2024-03-15 15:00:40 +0530

    s

--- app.py
+++ app.py
@@ -16,68 +16,63 @@ def allowed_file(filename):
 
 db = SQLAlchemy()
 
-class File(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    original_filename = db.Column(db.String(100))
-    filename = db.Column(db.String(100))
-    bucket = db.Column(db.String(100))
-    region = db.Column(db.String(100))
-    url = db.Column(db.String(1000))  # New column to store the pre-signed URL
+# class File(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     original_filename = db.Column(db.String(100))
+#     filename = db.Column(db.String(100))
+#     bucket = db.Column(db.String(100))
+#     region = db.Column(db.String(100))
+#     url = db.Column(db.String(1000))  # New column to store the pre-signed URL
+
+app.debug = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+
+db.init_app(app)
 
 
+@app.route('/', methods=['GET'])
+def index():
+    # Render the index.html template
+    return render_template('index.html')
 
 
-def create_app():
+@app.route("/upload", methods=["GET", "POST"])
+def handle_upload():
+    if request.method == "POST":
+        uploaded_file = request.files["file-to-save"]
+        if not allowed_file(uploaded_file.filename):
+            return "{uploaded_file.filename}" + "File is not allowed!"
 
-    app.debug = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+        new_filename = uploaded_file.filename + uuid.uuid4().hex + '.' + uploaded_file.filename.rsplit('.', 1)[1].lower()
 
-    db.init_app(app)
+        bucket_name = "hackathon2024-debugkings"  # Replace with your S3 bucket name
+        s3 = boto3.client('s3')
 
+        try:
+            # IAM role is used here for AWS credentials
+            s3.upload_fileobj(uploaded_file, bucket_name, new_filename)
+            # Generate a pre-signed URL for the uploaded file
+            file_url = s3.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': new_filename},
+                                                    ExpiresIn=3600)  # Link expires in 1 hour
+            res = trigger_lambda_function(new_filename)    
+            return jsonify({'report': res})
+        except NoCredentialsError:
+            return "Credentials are not available for AWS S3."
 
-    @app.route('/', methods=['GET'])
-    def index():
-        # Render the index.html template
-        return render_template('index.html')
+        # file = File(original_filename=uploaded_file.filename, filename=new_filename,
+        #             bucket=bucket_name, region="us-east-1", url=file_url)
 
+        # db.session.add(file)
+        # db.session.commit()
 
-    @app.route("/upload", methods=["GET", "POST"])
-    def handle_upload():
-        if request.method == "POST":
-            uploaded_file = request.files["file-to-save"]
-            if not allowed_file(uploaded_file.filename):
-                return "{uploaded_file.filename}" + "File is not allowed!"
+        # return redirect(url_for("index"))
+        
+        
+    ##files = File.query.all()
 
-            new_filename = uploaded_file.filename + uuid.uuid4().hex + '.' + uploaded_file.filename.rsplit('.', 1)[1].lower()
-
-            bucket_name = "hackathon2024-debugkings"  # Replace with your S3 bucket name
-            s3 = boto3.client('s3')
-
-            try:
-                # IAM role is used here for AWS credentials
-                s3.upload_fileobj(uploaded_file, bucket_name, new_filename)
-                # Generate a pre-signed URL for the uploaded file
-                file_url = s3.generate_presigned_url('get_object',
-                                                     Params={'Bucket': bucket_name,
-                                                             'Key': new_filename},
-                                                     ExpiresIn=3600)  # Link expires in 1 hour
-                res = trigger_lambda_function(new_filename)    
-                return jsonify({'report': res})
-            except NoCredentialsError:
-                return "Credentials are not available for AWS S3."
-
-            # file = File(original_filename=uploaded_file.filename, filename=new_filename,
-            #             bucket=bucket_name, region="us-east-1", url=file_url)
-
-            # db.session.add(file)
-            # db.session.commit()
-
-            # return redirect(url_for("index"))
-            
-            
-        ##files = File.query.all()
-
-        ##return render_template("index.html", files=files)
+    ##return render_template("index.html", files=files)
 
 
 
